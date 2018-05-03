sped up potential energy calculation significantly. Corrected initial conditions position draw (wasnt random on sphere) and added fixed_seed option

initialConditions/plummerSphere.py
@@ -1,6 +1,21 @@
 import numpy as np
 
-def plummer(Npart, a, m=1., G=1.):
+
+def rand_unit_vector(d, rand=np.random):
+    """
+    Returns d-dimensional random unit vector (norm = 1)
+    """
+    phi = rand.uniform(0, np.pi*2)
+    costheta = rand.uniform(-1, 1)
+    theta = np.arccos(costheta)
+    x = np.sin(theta)*np.cos(phi)
+    y = np.sin(theta)*np.sin(phi)
+    z = np.cos(theta)
+    vec = np.array([x, y, z])
+    return vec
+
+
+def plummer(Npart, a, m=1., G=1., seed=None):
     """
     Returns the positions and velocities of particles in the Plummer sphere
     input:
@@ -8,49 +23,47 @@ def plummer(Npart, a, m=1., G=1.):
         a - Plummer radius (scale parameter setting the cluster core)
         m - mass of particles (if single value, all particles will be initialized with the same mass; if array, each particles i will have mass m_i)
         G - Gravitational constant; by default, work in units where G=1
+        seed - random generator seed (int, optional: defaults to random numpy seed)
     output:
         [pos, vel] - [(Npart x 3), (Npart x 3)] of positions and velocities in cartesian coordinates
 
     """
-    pos = PlummerDist_3d_xyz(Npart, a)
+    if seed == None:
+        rand = np.random
+    else:
+        rand = np.random.RandomState(seed=seed)
+    pos = PlummerDist_3d_xyz(Npart, a, rand=rand)
     if np.size(m) == 1:
         M = Npart * m # if all particles have the same mass
     else:
         M = np.sum(m)
-    vel = velDist_Plummer(Npart, pos, M, a, G)
+    vel = velDist_Plummer(Npart, pos, M, a, G, rand=rand)
     return [pos, vel]
 
+
 ### Spatial Distribution for Plummer Model
-def PlummerDist_3d_xyz(Npart,a):
+def PlummerDist_3d_xyz(Npart, a, rand=np.random):
     """
     Initializes particles in 3d with Plummer density profile.
     input: 
         Npart - total number of particles to be initialized
         a - Plummer radius (scale parameter setting the cluster core)
+        rand - RandomState generator (optional: defaults to numpy.random)
     output:
         pos   - (Npart x 3) array of positions in cartesian coordinates
     """
     r = np.zeros((Npart))
-    phi = np.zeros((Npart))
-    theta = np.zeros((Npart))
-    x = np.zeros((Npart))
-    y = np.zeros((Npart))
-    z = np.zeros((Npart))
+    pos = np.zeros((Npart, 3))
     for i in range(Npart):
         # Let enclosed mass fraction f_mi be random uniform number between 0 and 1 
-        f_mi = np.random.uniform(0.,1.)
+        f_mi = rand.uniform(0., 1.)
         r[i] = a/np.sqrt(f_mi**(-2./3.)-1.)
-        # Let the angles be chosen uniformly at random
-        theta[i] = 2.*np.pi*np.random.uniform(0.,1.)
-        phi[i] = np.pi*np.random.uniform(0.,1.)   
-        x[i] = r[i]*np.sin(theta[i])*np.cos(phi[i])
-        y[i] = r[i]*np.sin(theta[i])*np.sin(phi[i])
-        z[i] = r[i]*np.cos(theta[i])
-    pos = np.array([x,y,z]).T
+        pos[i] = r[i]*rand_unit_vector(3, rand=rand)
     return pos
 
+
 ### Initial velocities for particles in the Plummer model
-def velEscape_Plummer(r,M,a,G):
+def velEscape_Plummer(r, M, a, G):
     """
     Returns the escape velocity of a particle in the Plummer model 
     at a radius r from the center of the Plummer sphere
@@ -62,11 +75,11 @@ def velEscape_Plummer(r,M,a,G):
     output:
         escape velocity of a particle at radius r inside a Plummer sphere
     """
-    r_abs = np.linalg.norm(r)
+    # r_abs = np.linalg.norm(r)
     pref = np.sqrt(2.*G*M/a)
     return pref*(1.+(r*r)/(a*a))**(-0.25)
 
-def rejTech_velPlummer(r,M,a,G):
+def rejTech_velPlummer(r, M, a, G, rand=np.random):
     """
     Uses the rejection technique to return a velocity modulus drawn at random 
     from the velocity distribution of particles at radius r in the Plummer model
@@ -75,6 +88,7 @@ def rejTech_velPlummer(r,M,a,G):
         M - total mass of the Plummer sphere
         a - Plummer radius (scale parameter setting the cluster core)
         G - gravitational constant
+        rand - RandomState generator (optional: defaults to numpy.random)
     output:
         absoulte velocity of a particle at radius r inside a Plummer sphere
     """
@@ -83,32 +97,19 @@ def rejTech_velPlummer(r,M,a,G):
     g0 = gmax
     while g0 > g_Plummer(x0):
         # 0 <= v <= v_esc or 0 <= x <= 1 where x = v/v_esc
-        x0 = np.random.uniform(0.,1.) 
+        x0 = rand.uniform(0., 1.) 
         # 0 <= g <= g_max
-        g0 = np.random.uniform(0.,gmax)
+        g0 = rand.uniform(0., gmax)
     return x0*velEscape_Plummer(r,M,a,G)
+
 
 def g_Plummer(x):
     return x*x * (1. - x*x)**3.5 
 
-def rand_unit_vector(d):
-    """
-    Returns d-dimensional random unit vector (norm = 1)
-    """
-    #r = np.random.randn(d)
-    #norm = np.sqrt(np.sum(r**2))
-    phi = np.random.uniform(0,np.pi*2)
-    costheta = np.random.uniform(-1,1)
-    theta = np.arccos(costheta)
-    x = np.sin(theta)*np.cos(phi)
-    y = np.sin(theta)*np.sin(phi)
-    z = np.cos(theta)
-    vec = np.array([x,y,z])
-    return vec
 
-def velDist_Plummer(Npart,r,M,a,G):
+def velDist_Plummer(Npart, r, M, a, G, rand=np.random):
     """
-    Returns velocities from the velocity distribution of particles 
+    Returns velocities from the velocity distribution of particles
     at radius r in the Plummer model
     input: 
         Npart - total number of particles to be initialized
@@ -116,20 +117,21 @@ def velDist_Plummer(Npart,r,M,a,G):
         M     - total mass of the Plummer sphere
         a     - Plummer radius (scale parameter setting the cluster core)
         G     - gravitational constant
+        rand - RandomState generator (optional: defaults to numpy.random)
     output:
         vel   - array of velocities of particles in Plummer model at radii r
     """
     vel = np.zeros((Npart,3))
     for i in range(Npart):
         r_abs = np.linalg.norm(r[i])
-        vel_mod = rejTech_velPlummer(r_abs,M,a,G)
-        vel[i,:] = rand_unit_vector(3)*vel_mod
+        vel_mod = rejTech_velPlummer(r_abs, M, a, G, rand=rand)
+        vel[i,:] = rand_unit_vector(3, rand=rand)*vel_mod
     return vel
 
-def circVel_Plummer(r,M,a,G):
+def circVel_Plummer(r, M, a, G, rand=np.random):
     N = r.shape[0]
     vel = np.zeros((N,3))
     for i in range(N):
-        vel[i,:] = rand_unit_vector(3)*velEscape_Plummer(np.linalg.norm(r[i]),M,a,G)/2.
+        vel[i, :] = rand_unit_vector(3, rand=rand)*velEscape_Plummer(np.linalg.norm(r[i]),M,a,G)/2.
     return vel
 

utils.py
@@ -3,11 +3,11 @@ from time import time
 from datetime import datetime, timedelta
 
 
-def compute_energy(pos, vel, mass, G=6.67e-11):
+def compute_energy(pos, vel, mass=None, G=1.):
     """
     Returns the total energy of the system defined by
-    input positions (pos), velocities (vel), and masses,
-    using units defined by choice of G.
+    input positions (pos), velocities (vel), and masses (defaults
+    to 1.0 for all particles), using units defined by choice of G.
 
     Total energy is the sum of potential and kinetic energy
     (see: compute_potential_energy and compute_kinetic_energy)
@@ -15,20 +15,23 @@ def compute_energy(pos, vel, mass, G=6.67e-11):
     Input:
        pos - positions (N x d)
        vel - velocities (N x d)
-       mass - masses (N)
-       G - Newton's Constant (optional. Default=6.67e-11)
+       mass - masses (optonal. Default: np.ones(N))
+       G - Newton's Constant (optional. Default=1.)
 
     Output:
        E - total energy (float)
     """
+    if mass is None:
+        mass = np.ones(len(pos))
     return compute_potential_energy(pos, mass, G=G) +\
         compute_kinetic_energy(vel, mass)
 
 
-def compute_potential_energy(pos, mass, G=6.67e-11):
+def compute_potential_energy(pos, mass=None, G=1.):
     """
     Returns the gravitational potential energy of the system defined by input
-    positions (pos) and masses, using units defined by choice of G.
+    positions (pos) and masses (defaults to 1.0 for all particles),
+    using units defined by choice of G.
     
     Potential energy is defined as:
     U = - sum_i ( sum_[j > i] ( G * mass[i] * mass[j] / r_ij))
@@ -36,42 +39,50 @@ def compute_potential_energy(pos, mass, G=6.67e-11):
 
     Input:
        pos - positions (N x d)
-       mass - masses (N)
-       G - Newton's Constant (optional. Default=6.67e-11)
+       mass - masses (optonal. Default: np.ones(N))
+       G - Newton's Constant (optional. Default=1.)
 
     Output:
        U - Gravitational potential energy (float)
     """
     pos = np.array(pos).astype(float)
-    mass = np.array(mass).astype(float)
     N_part = pos.shape[0]
+    if mass is None:
+        mass = np.ones(N_part)
+    else:
+        mass = np.array(mass).astype(float)
     assert mass.shape == (N_part,), ("input masses must match length of "
                                      "input positions")
     U = 0.
     for i in range(N_part):
-        for j in range(i+1, N_part):
-            r = np.sqrt(np.sum((pos[i] - pos[j])**2))
-            U -= G * mass[i] * mass[j] / r
+        m_i = mass[i]
+        m_j = mass[i+1:]
+        dr = np.linalg.norm(pos[i] - pos[i+1:], axis=1)
+        U -= np.sum(G * m_i * m_j / dr)
     return U
 
 
-def compute_kinetic_energy(vel, mass):
+def compute_kinetic_energy(vel, mass=None):
     """
     Returns the kinetic of the system defined by input
-    velocities and mass.
+    velocities and mass (defaults to 1.0 for all particles).
     
     Kinetic energy is defined as:
     K = 1/2 sum_i (mass[i] * ||vel[i]||**2)
 
     Input:
        vel - velocities (N x 3)
-       mass - masses (N)
+       mass - masses (optonal. Default: np.ones(N))
 
     Output:
        K - kinetic energy (float)
     """
     vel = np.array(vel).astype(float)
-    mass = np.array(mass).astype(float)
+    N_part = vel.shape[0]
+    if mass is None:
+        mass = np.ones(N_part)
+    else:
+        mass = np.array(mass).astype(float)
     N_part = vel.shape[0]
     assert mass.shape == (N_part,), ("input masses must match length of "
                                      "input velocities")