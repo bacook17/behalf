d69aaec4aa5a3ce42414acfb749fd2dd262b1a62
Ana-Roxana Pop <ana-roxana.pop@cfa.harvard.edu>
2018-05-06 00:17:04 -0400

    Speed-up analysis up to 64 cores

--- analysis/mpi_speedup_vs_ncores.py
+++ analysis/mpi_speedup_vs_ncores.py
@@ -4,27 +4,51 @@ import time
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    # after 100 iterations, Npart 1000, dt 0.1
+    # after 25 iterations, Npart 1000, dt 0.1
     Npart = 1000
     Nsteps = 1000
     dt = 10
 
-    NNsteps = 100
+    NNsteps = 500
 
-    NN = 5
-    serial_time = 140.*np.ones(NN)
-    parallel_time = np.array([ 140., 102. , 60., 46. ])
-    
-    ### TO-DO: read from .out file, finding the line with "NNsteps" and reading the computation time up to there
+    n_cores = [1, 2, 4, 8, 16,32, 64]
+    parallel_time = []
+    # Read serial time
+    outputFile = '../logs/serial_%d_%d_%d.out' %( Npart, Nsteps, dt)
+    with open(outputFile) as fopen:
+        lines = fopen.readlines()
+        lines = [x.strip() for x in lines]
+        temp = lines[NNsteps + 1].split("complete. ",1)
+        temp = temp[1].split(" seconds")
+        parallel_time.append(float(temp[0]))
+    # Read parallel time for ncores
+    for ncores in n_cores[1:]:
+        outputFile = '../logs/mpi_%d_%d_%d_%d.out' %(ncores, Npart, Nsteps, dt)
+        with open(outputFile) as fopen:
+            lines = fopen.readlines()
+        lines = [x.strip() for x in lines]
+        temp = lines[NNsteps + 1].split("complete. ",1)
+        temp = temp[1].split(" seconds")
+        parallel_time.append(float(temp[0]))
+
+    print 'Running time after %d steps for ncores:' %NNsteps
+    NN = np.size(n_cores)
+
+    print 'Serial time: %.2f seconds' %(parallel_time[0])
+    for i in range(1,NN):
+        print '%d cores: %.2f seconds' %(n_cores[i], parallel_time[i])
+
+    serial_time = parallel_time[0]*np.ones(NN)
 
     ratio = serial_time/parallel_time
-    k_array = [1, 2, 4, 8, 16]
+    
+    
 
     plt.figure()
-    plt.plot(k_array, ratio, '-ob')
+    plt.plot(n_cores, ratio, '-ob')
     plt.xlabel('Number of cores (same node)')
-    plt.ylabel('Speedup', fontsize=8)
-
+    plt.ylabel('Speedup',fontsize=8)
+    plt.xscale('log')
     plt.title('Speedup versus number of cores')
-    plt.savefig('mpi_speedup_vs_ncores_Npart%d_Nsteps%d_dt%d.png' %(Npart, Nsteps, dt), dpi=600)
+    plt.savefig('mpi_speedup_vs_ncores_Npart%d_Nsteps%d_dt%d_64.png' %(Npart, NNsteps, dt), dpi=600)
     plt.show()
