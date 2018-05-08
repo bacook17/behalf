9df70489af47579b4bd772b46373dac05b8cdf08
Ana-Roxana Pop <ana-roxana.pop@cfa.harvard.edu>
2018-05-08 01:29:26 -0400

    Add mpi scalings tests

---
+++ slurm_scripts/mpi_scalings/mpi_16-8_10000_100_100.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -p hernquist
+#SBATCH -J mpi_16-8_10000_100_100 # Job Name
+#SBATCH -n 16 # Number of MPI tasks
+#SBATCH -N 8 #ensure that all cores are on one machine
+#SBATCH -t 3-00:00 #runtime in D-HH:MM
+#SBATCH --mem-per-cpu 4000 #memory per MPI task
+#SBATCH -o ../../logs/%x.out #logs/%x.out
+#SBATCH -e ../../logs/%x.err #logs/%x.err
+#SBATCH --mail-type=BEGIN,END,FAIL #alert when done
+#SBATCH --mail-user=ana-roxana.pop@cfa.harvard.edu #Email to send to
+
+mpiexec -n $SLURM_NTASKS ../../bin/run_behalf.py --run-name $SLURM_JOB_NAME --clobber --verbose --N-parts 10000 --N-steps 100 --dt 0.01
+RESULT=${PIPESTATUS[0]}
+sacct -j "${SLURM_JOB_ID}".batch --format=JOBID%20,JobName,NTasks,AllocCPUs,AllocGRES,Partition,Elapsed,MaxRSS,MaxVMSize,MaxDiskRead,MaxDiskWrite,State 
+exit $RESULT
